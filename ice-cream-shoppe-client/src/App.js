e511ffb171f098bb4b76c0f06fbf32a2f46be163
Bonnie Schulkin <biz@bonnieschulkin.com>
2021-01-19 18:30:43 -0800

    Add react-bootstrap imports and static text

--- ice-cream-shoppe-client/src/App.js
+++ ice-cream-shoppe-client/src/App.js
@@ -1,8 +1,19 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import Alert from "react-bootstrap/Alert";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Spinner from "react-bootstrap/Spinner";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Container className="App">
+      <h1>The Ice Cream Shoppe</h1>
+      <h2>Today's Flavors</h2>
+    </Container>
+  );
 }
 
 export default App;
