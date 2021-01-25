7aebfd0611d4fcfeb43ae78538ce02ac3ae9ce0e
Bonnie Schulkin <biz@bonnieschulkin.com>
2021-01-25 10:54:26 -0800

    Final code

--- ice-cream-shoppe-client/src/App.js
+++ ice-cream-shoppe-client/src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import Alert from "react-bootstrap/Alert";
 import Col from "react-bootstrap/Col";
@@ -8,10 +10,48 @@ import Row from "react-bootstrap/Row";
 import Spinner from "react-bootstrap/Spinner";
 
 function App() {
+  const [flavorsData, setFlavorsData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [hasError, setHasError] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    axios.get("http://localhost:3030/flavors")
+    .then(response => {
+      setFlavorsData(response.data)
+      setLoading(false);
+    })
+    .catch(() => {
+      setLoading(false);
+      setHasError(true);
+    })
+  }, []);
+
+  const flavorsElements = flavorsData.map(flavor => (
+    <Col key={flavor.name} xs="12" sm="6" md="4" lg="3">
+      <img
+        className="icecream"
+        src={`http://localhost:3030/${flavor.imagePath}`}
+        alt={`${flavor.name} flavor`}
+        />
+    </Col>
+  ));
+
   return (
     <Container className="App">
       <h1>The Ice Cream Shoppe</h1>
       <h2>Today's Flavors</h2>
+      <Spinner
+        style={{ display: loading ? "block" : "none" }}
+        role="progressbar"
+        animation="grow"
+        aria-hidden={!loading}
+        aria-label="Loading"
+      />
+      <Alert style={{ display: hasError ? "block" : "none" }} variant="danger">
+        There was an error contacting the server. Try again later and stay cool!
+      </Alert>
+      <Row>{flavorsElements}</Row>
     </Container>
   );
 }
--- ice-cream-shoppe-client/src/App.test.js
+++ ice-cream-shoppe-client/src/App.test.js
@@ -1,4 +1,50 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import { server } from './mocks/server';
+import { rest } from 'msw';
 
-test.todo("some tests here");
+test("displays images from the server", async () => {
+  render(<App />);
+
+  // show loading spinner
+  const loadingSpinner = await screen.findByRole('progressbar');
+  expect(loadingSpinner).toBeVisible();
+
+  // load images from server
+  const images = await screen.findAllByRole("img", { name: /flavor$/i });
+  expect(images).toHaveLength(2);
+  
+  // loading spinner no longer shows
+  // const hiddenLoadingSpinner = screen.getByRole('progressbar', { hidden: true })
+  // expect(hiddenLoadingSpinner).not.toBeVisible();
+
+  const hiddenLoadingSpinner = screen.queryByRole('progressbar');
+  expect(hiddenLoadingSpinner).not.toBeInTheDocument();
+
+  // no error is visible
+  const error = screen.queryByRole('alert');
+  expect(error).not.toBeInTheDocument();
+});
+
+test("displays error upon error response from server", async () => {
+  server.resetHandlers(
+    rest.get("http://localhost:3030/flavors", 
+    (req, res, ctx) => res(ctx.status(500)))
+  );
+
+  render(<App />);
+
+  // show loading spinner
+   const loadingSpinner = screen.getByRole('progressbar');
+   expect(loadingSpinner).toBeVisible();
+
+  // show error banner
+  const error = await screen.findByRole("alert");
+  expect(error).toBeVisible();
+  // name option doesn't work for this particular dom
+  expect(error).toHaveTextContent("error contacting the server");
+
+  // expect loading spinner to have disappeared
+  const hiddenLoadingSpinner = screen.queryByRole('progressbar');
+  expect(hiddenLoadingSpinner).not.toBeInTheDocument();
+})
